014e322e9804e9e6471c52e35b1083b9016843bc
Thanwarat Chainam <gucode@Thanwarats-MacBook-Pro.local>
2026-01-11 12:10:16 +0700

    google analytic added

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,6 +33,20 @@ export default function RootLayout({ children }) {
             gtag('config', 'AW-17154230718');
           `}
         </Script>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
